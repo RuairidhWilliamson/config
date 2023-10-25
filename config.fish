cfed985b6863308cec88bde9b0723e6984287872
Ruairidh Williamson <williamsonruairidh@gmail.com>
2023-10-25 20:30:05 +0100

    Updated fish config to use gpg

--- config.fish
+++ config.fish
@@ -18,7 +18,10 @@ starship init fish | source
 
 alias ls="eza --icons"
 
-set -e SSH_AGENT_PID
-set SSH_AUTH_SOCK "$(gpgconf --list-dirs agent-ssh-socket)"
-set GPG_TTY $(tty)
-gpg-connect-agent updatestartuptty /bye >/dev/null
+set -e SSH_AUTH_SOCK
+set -U -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+
+set -x GPG_TTY (tty)
+
+gpgconf --launch gpg-agent
+echo UPDATESTARTUPTTY | gpg-connect-agent
