64e9344ca5f7c53f104220e28f0f8b303d0f428a
Ruairidh Williamson <williamsonruairidh@gmail.com>
2024-01-19 08:26:04 +0000

    Moved config into simpler home dir

--- copy.sh
+++ copy.sh
@@ -5,17 +5,5 @@ mkdir -p ~/.config
 echo "Adding .config/*"
 cp -r config/* ~/.config/
 
-echo "Adding tmux"
-cp ./tmux.conf ~/.tmux.conf
-
-echo "Adding gitconfig"
-cp ./gitconfig ~/.gitconfig
-
-echo "Adding global gitignore"
-cp ./gitignore ~/.gitignore
-
-echo "Adding bashrc"
-cp ./bashrc ~/.bashrc
-
-echo "Adding allowed signers"
-cp ./allowed_signers ~/.ssh/allowed_signers
+echo "Adding ~/*"
+cp -r home/.* ~/
